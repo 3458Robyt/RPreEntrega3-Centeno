32219665d2c220152764d38695ec10a9e1b53ad2
David Centeno <centeno70204@gmail.com>
2023-08-24 02:27:12 -0500

    firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ4zFljgtgvvF6FE3ho_oyUVq1-gIta4I",
+  authDomain: "rpreentrega3-centeno.firebaseapp.com",
+  projectId: "rpreentrega3-centeno",
+  storageBucket: "rpreentrega3-centeno.appspot.com",
+  messagingSenderId: "146432335734",
+  appId: "1:146432335734:web:d14ca3f6b82940f8d8a457",
+  measurementId: "G-ZGLWQN2WZT"
+};
+
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
